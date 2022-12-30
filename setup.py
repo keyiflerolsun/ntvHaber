9ffd4eb2787b7f05718c16279c1f5bbc9f0253ae
keyiflerolsun <keyiflerolsun@gmail.com>
2022-12-30 12:55:51 +0300

    🕊
    🕊

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from io         import open
 setup(
     # ? Genel Bilgiler
     name         = "ntvHaber",
-    version      = "0.4",
+    version      = "0.5",
     url          = "https://github.com/keyiflerolsun/ntvHaber",
     description  = "NTV Son Dakika Haberleri",
     keywords     = ["ntvHaber", "KekikAkademi", "keyiflerolsun"],
